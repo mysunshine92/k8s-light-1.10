bb313a9a4b6ac65226b44c8a4bbb1fa6d8534402
zhangxiaoyu-zidif <zhang.xiaoyu33@zte.com.cn>
2018-05-15 06:22:04 +0800

    delete cmd deploy

--- pkg/kubectl/cmd/create.go
+++ pkg/kubectl/cmd/create.go
@@ -108,7 +108,7 @@ func NewCmdCreate(f cmdutil.Factory, out, errOut io.Writer) *cobra.Command {
 	cmd.AddCommand(NewCmdCreateConfigMap(f, out))
 	cmd.AddCommand(NewCmdCreateServiceAccount(f, out))
 	cmd.AddCommand(NewCmdCreateService(f, out, errOut))
-	cmd.AddCommand(NewCmdCreateDeployment(f, out, errOut))
+	//cmd.AddCommand(NewCmdCreateDeployment(f, out, errOut))
 	cmd.AddCommand(NewCmdCreateClusterRole(f, out))
 	cmd.AddCommand(NewCmdCreateClusterRoleBinding(f, out))
 	cmd.AddCommand(NewCmdCreateRole(f, out))
